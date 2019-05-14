8a4dc801837a42ce4b3a0f4a9407ef74be8d6485
PranitaMusande <ppmusande07@gmail.com>
2019-05-14 23:01:04 +0530

    Task6

--- src/main/java/com/stackroute/BeanLifeCycleDemo.java
+++ src/main/java/com/stackroute/BeanLifeCycleDemo.java
@@ -10,27 +10,27 @@ public class BeanLifeCycleDemo implements InitializingBean, DisposableBean {
 
     public static void main(String gopal[]) {
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("BeanFile.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
 
         ((ClassPathXmlApplicationContext) applicationContext).registerShutdownHook();
         BeanLifeCycleDemo beanLifeCycleDemo = (BeanLifeCycleDemo) applicationContext.getBean("lifecycle");
 
-
     }
-
-    public void Init() {
+    public void Init ()
+    {
         System.out.println("Initialization");
     }
 
-    public void clean() {
+    public  void clean()
+    {
         System.out.println("custom destroy");
     }
-
-    public void destroy() {
+    public void destroy ()
+    {
         System.out.println("Destroy");
     }
 
-    public void afterPropertiesSet() throws Exception {
+    public void afterPropertiesSet () throws Exception {
         System.out.println("After properties");
 
     }
